3b8cbbce43e5c4052ae98cd70f75ef9bc87bfc4e
juliano <julianojd@ymail.com>
2022-06-30 17:04:03 -0300

    Set model values to view

--- dummyApp/Features/Home/Controllers/HomeViewController.swift
+++ dummyApp/Features/Home/Controllers/HomeViewController.swift
@@ -2,8 +2,22 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    let homeView = HomeView()
+    
+    override func loadView() {
+        view = homeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        homeView.setup(configuration: .init(message: "It's works, welcome!", formattedDate: getFormattedDate()))
+    }
+    
+    private func getFormattedDate() -> String {
+        let formatted = DateFormatter()
+        formatted.dateFormat = "EEEE, dd 'de' MMMM, YYYY 'em:' HH:mm:ss"
+        formatted.locale = Locale(identifier: "pt-br")
+        return formatted.string(from: Date())
     }
 }
 
